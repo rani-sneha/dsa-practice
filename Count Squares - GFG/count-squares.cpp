81764fdc3626606d6b3a4951a4a80522446ec1f9
Sneha Rani <sneharani0212@gmail.com>
2022-10-17 18:38:37 +0530

    Added solution - LeetHub

---
+++ Count Squares - GFG/count-squares.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        if(ceil(sqrt(N)) == floor(sqrt(N)))
+      return (pow(N, 0.5)-1);
+      else
+      return (pow(N, 0.5));
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
