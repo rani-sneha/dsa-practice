d55226fdccc34f9e61578fd085fdc84f51aaee3d
Sneha Rani <sneharani0212@gmail.com>
2022-11-02 11:16:09 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -20,20 +20,20 @@ class Solution
 {
     public:
     //Function to sort the array using bubble sort algorithm.
-    void bubbleSort(int arr[], int n)
-    {
+    void bubbleSort(int arr[], int n){
         int temp;
-        for(int i=0; i<n-1; i++){
-            
-            for(int j=1; j<n-i; j++){
-                
-                if(arr[j-1]> arr[j]){
-                    temp= arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                }
-            }
-        }
+         for(int i=0; i<n-1; i++)
+         {
+             for(int j=1; j<n-i; j++)
+             {
+                 if(arr[j-1]> arr[j])
+                 {
+                     temp=arr[j];
+                     arr[j]=arr[j-1];
+                     arr[j-1]=temp;
+                 }
+             }
+         }
     }
 };
 
